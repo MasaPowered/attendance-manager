cfba8547232ae555a0d5a38f18d67cd73c98412a
masa <hrkdx333@gmail.com>
2026-05-13 18:30:29 +0900

    ユーザー追加のデバッグ

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -165,6 +165,7 @@ class UserController extends Controller
             }
 
             if (empty($error_message)) {
+                dd('check!!!!!!!!!!!!!!!!!!!!!!!!!!!');
                 $res = User::create([
                     //2026.04.29 別の形でuserid追加する予定
                     //"id" => $request->id,
